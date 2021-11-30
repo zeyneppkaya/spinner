d4ad0557298d37a867b35bf592b5567f0003e426
Zeynep <zeynepkaya_26@hotmail.com>
2021-11-30 20:50:46 +0000

    make a spinner

--- spinner1.js
+++ spinner1.js
@@ -1,19 +1,14 @@
 let a = 200;
 
 for (let i = 0; i < 100; i++) {
-  setTimeout(() => {
-    process.stdout.write('\r |   ');
+  setTimeout(() => { process.stdout.write('\r |   ');
   }, a + (i * 1000));
-  setTimeout(() => {
-    process.stdout.write('\r /   ');
-  }, 2 * a + (i * 1000));
-  setTimeout(() => {
-    process.stdout.write('\r -   ');
-  }, 3 * a + (i * 1000));
-  setTimeout(() => {
-    process.stdout.write('\r \\   ');
-  }, 4 * a + (i * 1000));
-  setTimeout(() => {
-    process.stdout.write('\r |   ');
-  }, 5 * a + (i * 1000));
+  setTimeout(() => { process.stdout.write('\r /   ');
+  }, a * 2 + (i * 1000));
+  setTimeout(() => { process.stdout.write('\r -   ');
+  }, a * 3 + (i * 1000));
+  setTimeout(() => { process.stdout.write('\r \\   ');
+  }, a * 4 + (i * 1000));
+  setTimeout(() => { process.stdout.write('\r |   ');
+  }, a * 5 + (i * 1000));
 }
